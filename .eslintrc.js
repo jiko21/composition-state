53b0214ff831889eae8dd966de0de2fc38f5eda6
daikikojima <daikikojima0531@icloud.com>
2019-10-23 12:06:22 +0900

    A: eslint google code guideline

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,30 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
+  'root': true,
+  'env': {
+    node: true,
   },
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended',
-    '@vue/typescript'
+    '@vue/typescript',
+    'google',
   ],
-  rules: {
+  'rules': {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
+  'parserOptions': {
+    parser: '@typescript-eslint/parser',
   },
-  overrides: [
+  'overrides': [
     {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
-}
+        jest: true,
+      },
+    },
+  ],
+};
